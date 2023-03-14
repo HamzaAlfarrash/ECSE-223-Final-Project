3b95150f0c6ee2e83b942893889e8b92154f562f
souhailelhayani <souhail.200308@gmail.com>
2023-03-14 18:30:01 -0400

    5th method

--- src/test/java/ca/mcgill/ecse/snowshoetours/features/AddAndDeleteGearStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/snowshoetours/features/AddAndDeleteGearStepDefinitions.java
@@ -9,11 +9,13 @@ import java.util.List;
 import java.util.Map;
 import ca.mcgill.ecse.snowshoetours.application.SnowShoeToursApplication;
 import ca.mcgill.ecse.snowshoetours.model.SnowShoeTour;
+import ca.mcgill.ecse.snowshoetours.controller.GearController;
 import java.sql.Date;
 
 public class AddAndDeleteGearStepDefinitions {
 
   private SnowShoeTour sst;
+  private String error;
   
   /**
    * 
@@ -72,11 +74,17 @@ public class AddAndDeleteGearStepDefinitions {
     throw new io.cucumber.java.PendingException();
   }
 
+  /**
+   * @author Souhail El Hayani
+   * 
+   * @param string name
+   * @param string2 price per week
+   */
   @When("the manager attempts to add a new piece of gear with name {string} and price per week {string} \\(g5)")
   public void the_manager_attempts_to_add_a_new_piece_of_gear_with_name_and_price_per_week_g5(
       String string, String string2) {
     // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+   error = GearController.addGear(string, Integer.parseInt(string2));
   }
 
   @Then("a piece of gear shall exist with name {string} and price per week {string} \\(g5)")
