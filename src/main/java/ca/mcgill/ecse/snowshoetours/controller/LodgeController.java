29b335d9446288dcb8a794bf54a872abb0fabf69
YassineMimet1 <yassine.mimet@mail.mcgill.ca>
2023-03-19 21:43:29 -0400

    Lodge controller implemented but needs to be checked

--- src/main/java/ca/mcgill/ecse/snowshoetours/controller/LodgeController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/controller/LodgeController.java
@@ -1,14 +1,31 @@
 package ca.mcgill.ecse.snowshoetours.controller;
 
+import java.util.List;
+import ca.mcgill.ecse.snowshoetours.application.SnowShoeToursApplication;
+import ca.mcgill.ecse.snowshoetours.model.Lodge;
+import ca.mcgill.ecse.snowshoetours.model.SnowShoeTour;
+
 public class LodgeController {
-  // this method does not need to be implemented by a team with five team members
+  private static SnowShoeTour sst = SnowShoeToursApplication.getSnowShoeTour();
+  
   public static String addLodge(String name, String address, int nrStars) {
-    // TODO Implement the method, return error message (if any)
-    return "Not implemented!";
+    if (name == null || name.isEmpty()) return "Name cannot be empty";  
+    if (name == null || name.isEmpty()) return "Address cannot be empty";
+    
+    //Check the rating guys it should not be null
+    sst.addLodge(sst.addLodge(name, address, null));
+    return null;
   }
 
   // this method does not need to be implemented by a team with five team members
   public static void deleteLodge(String name) {
-    // TODO Implement the method
+    
+    List<Lodge> lodges = sst.getLodges();
+    Lodge aLodge = null;
+    for( Lodge lodge : lodges) {
+      if ( lodge.getName().equals(name)) aLodge = lodge;
+    }
+    
+    if(aLodge != null) aLodge.delete();
   }
 }
