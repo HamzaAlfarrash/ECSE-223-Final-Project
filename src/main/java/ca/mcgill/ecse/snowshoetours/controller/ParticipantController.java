bf77b95c9532f616d9cf184ee3d7e9444f76ee8b
wasifsomji <107225429+wasifsomji@users.noreply.github.com>
2023-03-20 17:44:26 -0400

    Update ParticipantController.java

--- src/main/java/ca/mcgill/ecse/snowshoetours/controller/ParticipantController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/controller/ParticipantController.java
@@ -14,8 +14,13 @@ public class ParticipantController {
 
   public static String addBookableItemToParticipant(String email, String bookableItemName) {
     // TODO Implement the method, return error message (if any)
-    return "Not implemented!";
+    if (email == "" || email == null) {
+      return "The email cannot be empty."
   }
+    if (bookableItemName = "" || bookableItemName == null) {
+      return "The bookable item name must not be empty. 
+  }
+    
 
   public static String removeBookableItemFromParticipant(String email, String bookableItemName) {
     // TODO Implement the method, return error message (if any)
