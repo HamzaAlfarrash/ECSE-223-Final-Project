231aeb9f0b7f83da975be3be5eb58e8a5532b46a
wasifsomji <107225429+wasifsomji@users.noreply.github.com>
2023-03-21 23:00:40 -0400

    Update LodgeController.java

--- src/main/java/ca/mcgill/ecse/snowshoetours/controller/LodgeController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/controller/LodgeController.java
@@ -11,47 +11,46 @@ public class LodgeController {
   
   public static String addLodge(String name, String address, int nrStars) {
     //input check
-    if (name == null || name.isEmpty()) return "Name cannot be empty";  
-    if (name == null || name.isEmpty()) return "Address cannot be empty";
+    if (name == null || name.isEmpty()) return "Name cannot be empty";  //checks validity of name
+    if (name == null || name.isEmpty()) return "Address cannot be empty"; //checks validity of ...
     
     LodgeRating rating;
     
     //get the rating from the number of stars
     switch(nrStars) {
       case 1:
-        rating = LodgeRating.OneStar;
+        rating = LodgeRating.OneStar; // if number of stars is 1
         break;
       case 2:
-        rating = LodgeRating.TwoStars;
+        rating = LodgeRating.TwoStars; // if number of stars is 2
         break;
       case 3:
-        rating = LodgeRating.ThreeStars;
+        rating = LodgeRating.ThreeStars; // if number of stars is 3
         break;
       case 4:
-        rating = LodgeRating.FourStars;
+        rating = LodgeRating.FourStars; // if number of stars is 4
         break;
       case 5:
-        rating = LodgeRating.FiveStars;
+        rating = LodgeRating.FiveStars; // if number of stars is 5
         break;
       default:
-        return "Rating should be between 1 and 5";
+        return "Rating should be between 1 and 5"; // if otherwise, means rating is not correct / in bounds
     }
     
     //Check the rating guys it should not be null
-    sst.addLodge(sst.addLodge(name, address, rating));
+    sst.addLodge(sst.addLodge(name, address, rating)); //adds to SnowSheTour
     
     return null;
   }
 
-  // this method does not need to be implemented by a team with five team members
   public static void deleteLodge(String name) {
     
-    List<Lodge> lodges = sst.getLodges();
+    List<Lodge> lodges = sst.getLodges(); // attains variable Lodges
     Lodge aLodge = null;
     for( Lodge lodge : lodges) {
-      if ( lodge.getName().equals(name)) aLodge = lodge;
+      if ( lodge.getName().equals(name)) aLodge = lodge; // checks through system and sets variables equal upon match
     }
     
-    if(aLodge != null) aLodge.delete();
+    if(aLodge != null) aLodge.delete(); // if aLodge is not null, it can be successfully deleted
   }
 }
