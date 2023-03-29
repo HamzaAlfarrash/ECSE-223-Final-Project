c4cc6556c933adc5d5409b250ef20b5e6d96e62a
Phil-March <philippe.marchand2@mail.mcgill.ca>
2023-03-29 18:43:42 -0400

    persistence sst started

---
+++ src/main/java/ca/mcgill/ecse/snowshoetours/persistence/SstPersistence.java
@@ -0,0 +1,34 @@
+package ca.mcgill.ecse.snowshoetours.persistence;
+
+import ca.mcgill.ecse.snowshoetours.application.SnowShoeToursApplication;
+import ca.mcgill.ecse.snowshoetours.model.SnowShoeTour;
+
+public class SstPersistence {
+
+	  private static String filename = "data.json";
+	  private static JsonSerializer serializer = new JsonSerializer("ca.mcgill.ecse.snowshoetours");
+
+	  public static void setFilename(String filename) {
+	    SstPersistence.filename = filename;
+	  }
+
+	  public static void save() {
+	    save(SnowShoeToursApplication.getSnowShoeTour());
+	  }
+
+	  public static void save(SnowShoeTour fms) {
+	    serializer.serialize(fms, filename);
+	  }
+
+	  public static SnowShoeTour load() {
+	    var sst = (SnowShoeTour) serializer.deserialize(filename);
+	    // model cannot be loaded - create empty BTMS
+	    if (sst == null) {
+	      sst = new SnowShoeTour(null,0,0); //TODO what are the input of the constructor??
+	    } else {
+	      sst.reinitialize();
+	    }
+	    return sst;
+	  }
+
+}
