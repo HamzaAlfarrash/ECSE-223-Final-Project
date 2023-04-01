4300de8bb64fd38150a83889d73f5409bb45d6b7
souhailelhayani <souhail.200308@gmail.com>
2023-03-31 21:02:57 -0400

    initiate() in controller

--- src/main/java/ca/mcgill/ecse/snowshoetours/controller/SnowShoeTourStateMachineController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/controller/SnowShoeTourStateMachineController.java
@@ -2,9 +2,11 @@ package ca.mcgill.ecse.snowshoetours.controller;
 
 import java.util.List;
 import ca.mcgill.ecse.snowshoetours.application.SnowShoeToursApplication;
+import ca.mcgill.ecse.snowshoetours.model.Guide;
 import ca.mcgill.ecse.snowshoetours.model.Participant;
 import ca.mcgill.ecse.snowshoetours.model.Participant.Status;
 import ca.mcgill.ecse.snowshoetours.model.SnowShoeTour;
+import ca.mcgill.ecse.snowshoetours.model.Tour;
 
 public class SnowShoeTourStateMachineController {
   
@@ -35,6 +37,20 @@ public class SnowShoeTourStateMachineController {
    * @return
    */
   public static String initiate() {
+    SnowShoeTour sst = SnowShoeToursApplication.getSnowShoeTour();
+    List<Participant> list = sst.getParticipants();
+    List<Guide> guides = sst.getGuides();
+    int id = 0; //will be used for id of a tour
+    int i = 0;
+    //for each participant, assign a Tour to it with matching startweek and endweek
+    for(Participant par : list) {
+      int nOfWeeks = par.getNrWeeks();
+      int weekAvailableFrom = par.getWeekAvailableFrom();
+      id++;
+      Tour tour = sst.addTour(id,weekAvailableFrom,weekAvailableFrom + nOfWeeks -1,sst.getGuide(i%guides.size())); //assign randomly a guide to the tour, starting from index 0 in the guide list
+      par.assign(tour); //assign tour to participant, state is not "Assigned"
+      i++;
+    }
     return "";
   }
   
