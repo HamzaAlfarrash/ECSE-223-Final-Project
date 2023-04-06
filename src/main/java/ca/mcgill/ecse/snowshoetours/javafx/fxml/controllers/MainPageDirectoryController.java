1b759215bee1180487b5c5045fe4fe22d54b0158
Phil-March <philippe.marchand2@mail.mcgill.ca>
2023-04-05 22:44:41 -0400

    UI SSTMainPage overview

--- src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/controllers/MainPageDirectoryController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/controllers/MainPageDirectoryController.java
@@ -1,6 +1,40 @@
 package ca.mcgill.ecse.snowshoetours.javafx.fxml.controllers;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.Parent;
+
 public class MainPageDirectoryController {
 	
-
+	private Button GoViewSnowShoeToursDetailedButton;
+	private Button GoUpdateShowShoeToursButton;
+	private Button GoAddDeleteLodgeButton;
+	private Button GoInitiateTheShowShoeingTourCreationButton;
+	private Button GoAddDeleteComboButton;
+	private Button GoAddDeleteGearButton;
+	private Button GoAddDeleteGearToComboButton;
+	private Button GoAddRemoveBookedItemButton;
+	private Button GoRegisterDeleteGuideButton;
+	private Button GoRegisterDeleteParticipantButton;
+	private Button GoPayParticipantButton;
+	private Button GoStartFinishCancelParticipantTripButton;
 }
+	
+//	public void GoViewSnowShoeToursDetailedButtonClicked(ActionEvent event) {
+//		try {
+////		   Parent 
+////		    } catch (IOException e) {
+////		      e.printStackTrace();
+//		   
+//		
+//	}
+//	}
+//	}
