cf51e25598f97a57860de1f8bfb8e3f94342f091
wasifsomji <wasifsomji@gmail.com>
2023-04-08 22:01:52 -0400

    controller method wasif

--- src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/controllers/StartFinishCancelParticipantTripController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/controllers/StartFinishCancelParticipantTripController.java
@@ -2,13 +2,31 @@ package ca.mcgill.ecse.snowshoetours.javafx.fxml.controllers;
 
 import java.io.IOException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 public class StartFinishCancelParticipantTripController {
+	@FXML
+	private Button backButton; 
+	
+	@FXML
+	private Button startTripButton;
+	
+	@FXML
+	private ChoiceBox<String> selectParticipantID; 
+	
+	@FXML
+	private Button cancelTripButton;
+	
+	@FXML
+	private Button finishTripButton; 
+	
   public void GoBackButtonClicked(ActionEvent event) {
     try {
       Parent ViewSnowShoeToursDetailedParent = FXMLLoader.load(getClass().getResource("../MainPage.fxml"));
