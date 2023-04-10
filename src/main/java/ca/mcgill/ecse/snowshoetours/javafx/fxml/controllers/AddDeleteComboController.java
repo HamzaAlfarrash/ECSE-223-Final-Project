487122236410d402d5125814d56768da0902389b
souhailelhayani <souhail.200308@gmail.com>
2023-04-10 19:25:50 -0400

    quick fized for add/delete combo file

--- src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/controllers/AddDeleteComboController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/controllers/AddDeleteComboController.java
@@ -17,81 +17,87 @@ import javafx.stage.Stage;
 
 
 public class AddDeleteComboController {
-	
-	@FXML
-	private TextField addComboNameTextField;
-	@FXML
-	private TexField discountTextField;
-	@FXML
-	private Button clearACSectionButton;
-	@FXML
-	private Button ACButton;
-	@FXML
-	private Button back;
-	@FXML
-	private ChoiceBox<String> deleteComboChoiceBox;
-	@FXML
-	private Button deleteComboButton;
-	
-	
-	/**
-	 * @author Hamza Alfarrash
-	 */
-	@FXML
-	  public void initialize() {
-	    deleteComboBox.setItems(ViewUtils.getCombo());
-	    deleteComboChoiceBox.setValue(null);
-	    deleteComboChoiceBox.addEventHandler(SSTFxmlView.REFRESH_EVENT, e -> {
-	      deleteComboChoiceBox.setItems(ViewUtils.getCombo());
-	      deleteComboChoiceBox.setValue(null);
-	    });
-	    SSTFxmlView.getInstance().registerRefreshEvent(deleteComboChoiceBox);
-	  }
-	
-	/**
-	 * @author Hamza Alfarrash
-	 * @param event
-	 */
-	@FXML
-	  public void ClearACClicked(ActionEvent event) {
-		addComboNameTextField.clear();
-		discountTextField.clear();
-	  }
-	
-	/**
-	 * @author Hamza Alfarrash
-	 * @param event
-	 */
-	@FXML
-	  public void ACButtonClicked(ActionEvent event) {
-	    String combo = addComboNameTextField.getText();
-	    String discount = discountTextField.getText();
 
-	    if (ViewUtils.callController(GearController.addCombo(combo, discount))) {
-	      addComboNameTextField.clear();
-		  discountTextField.clear();
-	      SSTFxmlView.getInstance().refresh();
-	    }
-	  }
-	
-	/**
-	 * @author Hamza Alfarrash
-	 * @param event
-	 */
-	@FXML
-	  public void deleteComboClicked(ActionEvent event) {
-	    GearController.deleteCombo(deleteComboChoiceBox.getValue());
-	    deleteComboChoiceBox.setValue(null);
-	    SSTFxmlView.getInstance().refresh();
-	  }
-  
-	/**
-	 * @author Hamza Alfarrash
-	 * @param event
-	 */
+  @FXML
+  private TextField addComboNameTextField;
+  @FXML
+  private TextField discountTextField;
+  @FXML
+  private Button clearACSectionButton;
+  @FXML
+  private Button ACButton;
+  @FXML
+  private Button back;
+  @FXML
+  private ChoiceBox<String> deleteComboChoiceBox;
+  @FXML
+  private Button deleteComboButton;
+
+
+  /**
+   * @author Hamza Alfarrash
+   */
+  @FXML
+  public void initialize() {
+    deleteComboChoiceBox.setItems(ViewUtils.getCombo());
+    deleteComboChoiceBox.setValue(null);
+    deleteComboChoiceBox.addEventHandler(SSTFxmlView.REFRESH_EVENT, e -> {
+      deleteComboChoiceBox.setItems(ViewUtils.getCombo());
+      deleteComboChoiceBox.setValue(null);
+    });
+    SSTFxmlView.getInstance().registerRefreshEvent(deleteComboChoiceBox);
+  }
+
+  /**
+   * @author Hamza Alfarrash
+   * @param event
+   */
+  @FXML
+  public void ClearACClicked(ActionEvent event) {
+    addComboNameTextField.clear();
+    discountTextField.clear();
+  }
+
+  /**
+   * @author Hamza Alfarrash
+   * @param event
+   */
+  @FXML
+  public void ACButtonClicked(ActionEvent event) {
+    try {
+      String combo = addComboNameTextField.getText();
+      int discount = Integer.parseInt(discountTextField.getText());
+
+      if (ViewUtils.callController(GearController.addCombo(combo, discount))) {
+        addComboNameTextField.clear();
+        discountTextField.clear();
+        SSTFxmlView.getInstance().refresh();    
+      }
+    } catch (NumberFormatException e) {
+      ViewUtils.showError("Invalid discount format");
+    }
+
+  }
+
+  /**
+   * @author Hamza Alfarrash
+   * @param event
+   */
+  @FXML
+  public void deleteComboClicked(ActionEvent event) {
+    GearController.deleteCombo(deleteComboChoiceBox.getValue());
+    deleteComboChoiceBox.setValue(null);
+    SSTFxmlView.getInstance().refresh();
+  }
+
+  /**
+   * @author Hamza Alfarrash
+   * @param event
+   */
   public void GoBackButtonClicked(ActionEvent event) {
     try {
-      Parent ViewSnowShoeToursDetailedParent = FXMLLoader.load(getClass().getResource("../MainPage.fxml"));
+      Parent ViewSnowShoeToursDetailedParent =
+          FXMLLoader.load(getClass().getResource("../MainPage.fxml"));
       Scene ViewSnowShoeToursDetailedParentScene = new Scene(ViewSnowShoeToursDetailedParent);
       Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
       window.setScene(ViewSnowShoeToursDetailedParentScene);
