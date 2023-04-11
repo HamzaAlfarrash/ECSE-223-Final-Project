93a6def36e8dec488576d9d50a222a0d1eeec2cb
souhailelhayani <souhail.200308@gmail.com>
2023-04-11 17:28:46 -0400

    push before merge

--- src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/MainPageController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/MainPageController.java
@@ -3,13 +3,9 @@ package ca.mcgill.ecse.snowshoetours.javafx.fxml;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Node;
