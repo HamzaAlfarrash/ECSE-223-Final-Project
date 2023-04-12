78d2fbe249af7afefc33fd5f68ffa2973be0ca0e
wasifsomji <wasifsomji@gmail.com>
2023-04-11 20:39:27 -0400

    partly done method

--- src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/controllers/StartFinishCancelParticipantTripController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/controllers/StartFinishCancelParticipantTripController.java
@@ -1,6 +1,10 @@
 package ca.mcgill.ecse.snowshoetours.javafx.fxml.controllers;
 
 import java.io.IOException;
+
+import ca.mcgill.ecse.snowshoetours.controller.LodgeController;
+import ca.mcgill.ecse.snowshoetours.controller.SnowShoeTourStateMachineController;
+import ca.mcgill.ecse.snowshoetours.javafx.fxml.SSTFxmlView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,9 +13,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class StartFinishCancelParticipantTripController {
+	
 	@FXML
 	private Button backButton; 
 	
@@ -26,6 +32,13 @@ public class StartFinishCancelParticipantTripController {
 	
 	@FXML
 	private Button finishTripButton; 
+	
+	@FXML
+	private ChoiceBox<Integer> startWeek; // need to implement this on UI, won't work right now
+	
+	@FXML
+	private Button clearButton; // need to implement this on UI, won't work right now
+	
 	
 	/**
 	 * @author Yassine Mimet
@@ -48,30 +61,50 @@ public class StartFinishCancelParticipantTripController {
      * @author Wasif Somji
      */
     public void initialize() {
-  	  // to be completed 
+    	selectParticipantID.setItems(ViewUtils.getParticipant());
+        selectParticipantID.setValue(null);
+        selectParticipantID.addEventHandler(SSTFxmlView.REFRESH_EVENT, e->{
+          selectParticipantID.setItems(ViewUtils.getParticipant());
+          selectParticipantID.setValue(null);
+        });
+          SSTFxmlView.getInstance().registerRefreshEvent(selectParticipantID);
+      }
+    
+    
+    
+    @FXML
+    /**
+     * @author Wasif Somji
+     */// start the trip for a certain number of weeks
+    public void startTripButtonClicked() {
+  	  int numWeeks =(startWeek.getValue()); 
+  	if(ViewUtils.successful(SnowShoeTourStateMachineController.startTourForWeek(numWeeks))) {
+  	    selectParticipantID.setValue(null);
+  	    SSTFxmlView.getInstance().refresh();
+  	  }
     }
     
     @FXML
     /**
      * @author Wasif Somji
-     */
-    public void startTrip() {
-  	  // to be completed
+     */// cancel the trip
+    public void cancelTripButtonClicked() {
+  	  String deleteParticipantName = selectParticipantID.getValue(); 
+  	if(ViewUtils.successful(SnowShoeTourStateMachineController.cancelTrip(deleteParticipantName))) {
+  	    selectParticipantID.setValue(null);
+  	    SSTFxmlView.getInstance().refresh();
+  	  }
     }
     
     @FXML
     /**
      * @author Wasif Somji
-     */
-    public void cancelTrip() {
-  	  // to be completed
-    }
-    
-    @FXML
-    /**
-     * @author Wasif Somji
-     */
+     */// finish the trip
     public void finishTrip() {
-  	  // to be completed
+  	  String finishParticipantName = selectParticipantID.getValue(); 
+  	if(ViewUtils.successful(SnowShoeTourStateMachineController.finishTour(finishParticipantName))) {
+  	    selectParticipantID.setValue(null);
+  	    SSTFxmlView.getInstance().refresh();
+  	  }
     }
 }
