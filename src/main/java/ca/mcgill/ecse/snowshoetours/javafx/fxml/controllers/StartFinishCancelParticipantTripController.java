a5b0b63729887afeb06c55c08e457da25710460a
souhailelhayani <souhail.200308@gmail.com>
2023-04-13 10:48:23 -0400

    final changes

--- src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/controllers/StartFinishCancelParticipantTripController.java
+++ src/main/java/ca/mcgill/ecse/snowshoetours/javafx/fxml/controllers/StartFinishCancelParticipantTripController.java
@@ -16,114 +16,122 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class StartFinishCancelParticipantTripController {
-	
-	@FXML
-	private Button backButton; 
-	
-	@FXML
-	private Button startTripButton;
-	
-	@FXML
-	private ChoiceBox<String> selectParticipantID; 
-	
-	@FXML
-	private Button cancelTripButton;
-	
-	@FXML
-	private Button finishTripButton; 
-	
-	@FXML
-	private TextField startWeekTextField; 
-	
-	@FXML
-	private Button clearButton; 
-	
-	
-	/**
-	 * @author Yassine Mimet
-	 * @param event
-	 */
-    public void GoBackButtonClicked(ActionEvent event) {
-      try {
-        Parent ViewSnowShoeToursDetailedParent = FXMLLoader.load(getClass().getResource("../MainPage.fxml"));
-        Scene ViewSnowShoeToursDetailedParentScene = new Scene(ViewSnowShoeToursDetailedParent);
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(ViewSnowShoeToursDetailedParentScene);
-        window.show();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
+
+  @FXML
+  private Button backButton;
+
+  @FXML
+  private Button startTripButton;
+
+  @FXML
+  private ChoiceBox<String> selectParticipantID;
+
+  @FXML
+  private Button cancelTripButton;
+
+  @FXML
+  private Button finishTripButton;
+
+  @FXML
+  private TextField startWeekTextField;
+
+  @FXML
+  private Button clearButton;
+
+
+  /**
+   * @author Yassine Mimet
+   * @param event
+   */
+  public void GoBackButtonClicked(ActionEvent event) {
+    try {
+      Parent ViewSnowShoeToursDetailedParent =
+          FXMLLoader.load(getClass().getResource("../MainPage.fxml"));
+      Scene ViewSnowShoeToursDetailedParentScene = new Scene(ViewSnowShoeToursDetailedParent);
+      Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+      window.setScene(ViewSnowShoeToursDetailedParentScene);
+      window.show();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    
-    @FXML
-    /**
-     * @author Wasif Somji
-     */
-    public void initialize() {
+  }
+
+  @FXML
+  /**
+   * @author Wasif Somji
+   */
+  public void initialize() {
+    selectParticipantID.setItems(ViewUtils.getParticipant());
+    selectParticipantID.setValue(null);
+    selectParticipantID.addEventHandler(SSTFxmlView.REFRESH_EVENT, e -> {
       selectParticipantID.setItems(ViewUtils.getParticipant());
       selectParticipantID.setValue(null);
-      selectParticipantID.addEventHandler(SSTFxmlView.REFRESH_EVENT, e->{
-        selectParticipantID.setItems(ViewUtils.getParticipant());
-        selectParticipantID.setValue(null);
-      });
-      SSTFxmlView.getInstance().registerRefreshEvent(selectParticipantID);
+    });
+    SSTFxmlView.getInstance().registerRefreshEvent(selectParticipantID);
+  }
+
+
+
+  @FXML
+  /**
+   * @author Wasif Somji || Yassine Mimet
+   */// start the trip for a certain number of weeks
+  public void startTripButtonClicked() {
+    String sWeek = (startWeekTextField.getText());
+    int startWeekTour = -1;
+    try {
+      startWeekTour = Integer.parseInt(sWeek);
+    } catch (NumberFormatException e) {
+      ViewUtils.showError("Enter an Integer");
     }
-    
-    
-    
-    @FXML
-    /**
-     * @author Wasif Somji || Yassine Mimet
-     */// start the trip for a certain number of weeks
-    public void startTripButtonClicked() {
-      String sWeek = (startWeekTextField.getText());
-      int startWeekTour = -1;
-      try {
-          startWeekTour = Integer.parseInt(sWeek);
-      } catch (NumberFormatException e) {
-          ViewUtils.showError("Enter an Integer");
-      }
-  	  if(ViewUtils.successful(SnowShoeTourStateMachineController.startTourForWeek(startWeekTour))) {
-  	    selectParticipantID.setValue(null);
-  	    startWeekTextField.clear();
-  	    SSTFxmlView.getInstance().refresh();
-  	  }
-    }
-    
-    @FXML
-    /**
-     * @author Wasif Somji || Yassine Mimet
-     */// cancel the trip
-    public void cancelTripButtonClicked() {
-      String ParticipantName = selectParticipantID.getValue();
-      String stWeek = startWeekTextField.getText();
-      if(!stWeek.isEmpty()) ViewUtils.makePopupWindow("error", "Do not fill the start week field when cancelling a trip");
-      if(ViewUtils.successful(SnowShoeTourStateMachineController.cancelTrip(ParticipantName))) {
-    	selectParticipantID.setValue(null);
-    	startWeekTextField.clear();
-    	SSTFxmlView.getInstance().refresh();
-      }
-    }
-    
-    @FXML
-    /**
-     * @author Wasif Somji || Yassine Mimet
-     */// finish the trip
-    public void finishTripButtonClicked() {
-  	  String ParticipantName = selectParticipantID.getValue(); 
-  	String stWeek = startWeekTextField.getText();
-    if(!stWeek.isEmpty()) ViewUtils.makePopupWindow("error", "Do not fill the start week field when finishing a trip");
-  	  if(ViewUtils.successful(SnowShoeTourStateMachineController.finishTour(ParticipantName))) {
-  	    selectParticipantID.setValue(null);
-  	    startWeekTextField.clear();
-  	    SSTFxmlView.getInstance().refresh();
-  	  }
-    }
-   /**
-    * @author Yassine Mimet
-    */
-    public void clearButtonClicked() {
+    if (ViewUtils.successful(SnowShoeTourStateMachineController.startTourForWeek(startWeekTour))) {
       selectParticipantID.setValue(null);
       startWeekTextField.clear();
+      SSTFxmlView.getInstance().refresh();
     }
+  }
+
+  @FXML
+  /**
+   * @author Wasif Somji || Yassine Mimet
+   */// cancel the trip
+  public void cancelTripButtonClicked() {
+    String ParticipantName = selectParticipantID.getValue();
+    String stWeek = startWeekTextField.getText();
+    if (!stWeek.isEmpty()) {
+      ViewUtils.makePopupWindow("error", "Do not fill the start week field when cancelling a trip");
+    }
+
+    else if (ViewUtils.successful(SnowShoeTourStateMachineController.cancelTrip(ParticipantName))) {
+      selectParticipantID.setValue(null);
+      startWeekTextField.clear();
+      SSTFxmlView.getInstance().refresh();
+    }
+  }
+
+  @FXML
+  /**
+   * @author Wasif Somji || Yassine Mimet
+   */// finish the trip
+  public void finishTripButtonClicked() {
+    String ParticipantName = selectParticipantID.getValue();
+    String stWeek = startWeekTextField.getText();
+    if (!stWeek.isEmpty()) {
+      ViewUtils.makePopupWindow("error", "Do not fill the start week field when finishing a trip");
+
+    } else if (ViewUtils
+        .successful(SnowShoeTourStateMachineController.finishTour(ParticipantName))) {
+      selectParticipantID.setValue(null);
+      startWeekTextField.clear();
+      SSTFxmlView.getInstance().refresh();
+    }
+  }
+
+  /**
+   * @author Yassine Mimet
+   */
+  public void clearButtonClicked() {
+    selectParticipantID.setValue(null);
+    startWeekTextField.clear();
+  }
 }
